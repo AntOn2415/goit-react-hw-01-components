eb6c0e31944e26fcf5ede948e8bc84116c2563ad
AntOnAntOff <povanton@gmail.com>
2023-05-03 23:00:29 +0300

    форматує документи

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,18 @@
 import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
-export  default function FriendListItem({isOnline, avatar, name,}) {
+export default function FriendListItem({ isOnline, avatar, name }) {
   return (
-  <li className={css.item}>
-  <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
-  <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-  <p className={css.name}>{name}</p>
-</li>
-  )
+    <li className={css.item}>
+      <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
 }
 
 FriendListItem.propTypes = {
   avatar: PropTypes.string,
   name: PropTypes.string,
-  isOnline: PropTypes.bool
-}
+  isOnline: PropTypes.bool,
+};
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,37 +1,35 @@
 import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
-export  default function Profile({username, tag, location, avatar, stats}) {
+export default function Profile({ username, tag, location, avatar, stats }) {
   return (
-<div className={css.profile}>
-  <div className={css.description}>
-    <img
-      src={avatar}
-      alt="User avatar"
-      className={css.avatar}
-    />
-    <p className={css.name}>{username}</p>
-    <p className={css.tag}>@{tag}</p>
-    <p className={css.location}>{location}</p>
-  </div>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
 
-  <ul className={css.stats}>
-    <li className={css.statsItem}>
-      <span className={css.label}>Followers</span>
-      <span className={css.quantity}>{stats.followers.toLocaleString()}</span>
-    </li>
-    <li className={css.statsItem}>
-      <span className={css.label}>Views</span>
-      <span className={css.quantity}>{stats.views.toLocaleString()}</span>
-    </li>
-    <li className={css.statsItem}>
-      <span className={css.label}>Likes</span>
-      <span className={css.quantity}>{stats.likes.toLocaleString()}</span>
-    </li>
-  </ul>
-</div>
-  )
-} 
+      <ul className={css.stats}>
+        <li className={css.statsItem}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>
+            {stats.followers.toLocaleString()}
+          </span>
+        </li>
+        <li className={css.statsItem}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views.toLocaleString()}</span>
+        </li>
+        <li className={css.statsItem}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes.toLocaleString()}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
@@ -39,4 +37,4 @@ Profile.propTypes = {
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
   stats: PropTypes.object,
-}
+};
